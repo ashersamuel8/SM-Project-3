7a44eac82599425e4d503a3336c1a9ae01073f4b
Samuel K <ashersamuel8@gmail.com>
2021-10-28 20:14:54 -0400

    layout of add student tab pane

--- src/main/java/com/project3/Project_3_Group_81/RunProject3.java
+++ src/main/java/com/project3/Project_3_Group_81/RunProject3.java
@@ -11,8 +11,8 @@ public class RunProject3 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RunProject3.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+        stage.setTitle("Roster");
         stage.setScene(scene);
         stage.show();
     }
